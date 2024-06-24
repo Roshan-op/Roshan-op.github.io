dabade9c6cf8f2693384db4572d0f925fb0ca1c7
[SLEEPY_HEAD] <71368790+Roshan-op@users.noreply.github.com>
2024-06-24 09:43:40 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Photoshop Desigine", "Creative ieda development", "Content writingg", "Figma designe", "web development"],
+    strings: ["Photoshop Design", "Creative ieda development", "Content writing", "Figma design", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
